1a6eb30460bf6265a3ad99a1bdd53c5c568ffc9e
atelierleo <leonard.lepage@gmail.com>
2026-01-04 01:15:01 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+const display = document.getElementById("display");
+let current = "";
+
+document.querySelectorAll(".buttons button").forEach(btn => {
+    btn.addEventListener("click", () => handleClick(btn.dataset.value));
+});
+
+function handleClick(value) {
+    if (value === "=") {
+        try {
+            current = eval(current).toString();
+            display.textContent = current;
+        }   catch {
+            display.textContent = "Error";
+            current = "";
+        }
+        return;
+    }
+
+    current += value;
+    display.textContent = current;
+}
